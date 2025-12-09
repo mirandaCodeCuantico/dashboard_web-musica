bbdc74b67bc82309bb9f51429793585c3f2fd239
HuevoCosmico <juanmir1002@gmail.com>
2025-12-09 00:23:14 -0500

    Inicio del proyecto final de Analisis de datos, en este commit, se suben los primeros pasos de la limpieza del dataset

---
+++ dashboard/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Dashboard",
+    page_icon = "📊",
+    layout = "wide"
+)
+
+st.title("📊Dashboard Musica y Salud Mental")
+st.markdown("Proyecto de Analisis de Datos - Musica y Salud Mental")
+
+st.info("Usa el menu lateral para navegar")
+
